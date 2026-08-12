7e5e14132c535604cd9e19169267d891ee0f4fc7
synthorlando <synth0rlando@pm.me>
2026-08-11 23:27:56 -0400

    Update autologin.sh

--- autologin.sh
+++ autologin.sh
@@ -26,6 +26,34 @@ chmod 644 "$OVERRIDE_FILE"
 # cargar systemd otra vez
 systemctl daemon-reload
 
+# configurar systemd para syncthing
+echo "Configurando Syncthing..."
+cat > /etc/systemd/system/syncthing@.service <<EOF
+[Unit]
+Description=Sincronizacion de archivos
+Documentation=man:syncthing(1)
+After=network.target
+
+[Service]
+Type=simple
+User=%i
+Group=%i
+ExecStart=/usr/bin/syncthing serve --home=/home/%i/.config/syncthing
+Restart=on-failure
+RestartSec=5
+SuccessExitStatus=3 4
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable syncthing@$USERNAME
+systemctl start syncthing@$USERNAME
+
+# Comprobar
+echo "Revisando status de Syncthing..."
+systemctl status syncthing@$USERNAME --no-pager
+
 # reiniciar getty
 systemctl restart getty@tty1
 
